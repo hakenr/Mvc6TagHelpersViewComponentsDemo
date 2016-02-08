47d5dfc0bf25349e21e1624a4d204f819388f196
Robert Haken <haken@havit.cz>
2016-02-09 00:41:14 +0100

    Sample TagHelpers

---
+++ src/TagHelpersAndViewComponents/TagHelpers/AutoLinkerWwwTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Razor.TagHelpers;
+
+namespace TagHelpersAndViewComponents.TagHelpers
+{
+	[HtmlTargetElement("p")]
+	public class AutoLinkerWwwTagHelper : TagHelper
+	{
+		public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+		{
+			var childContent = await output.GetChildContentAsync();
+			// Find Urls in the content and replace them with their anchor tag equivalent.
+			output.Content.SetHtmlContent(Regex.Replace(
+				childContent.GetContent(),
+				 @"\b(www\.)(\S+)\b",
+				 "<a target=\"_blank\" href=\"http://$0\">$0</a>"));  // www version
+		}
+	}
+}
